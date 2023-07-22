c68382c26c49e982f6381e6fcfa8f4762aedd743
r567tw <r567tw@gmail.com>
2023-07-22 11:51:32 +0800

    covert static html

--- single.php
+++ single.php
@@ -1,12 +1,14 @@
-<?php 
-    while (have_posts()) {
-        the_post();
-        echo "<a href='";
-        echo the_permalink();
-        echo "'>";
-        echo the_title();
-        echo "</a>";
+<?php
+  
+  get_header();
 
-        echo the_content();
-    }
+  while(have_posts()) {
+    the_post(); ?>
+    <h2><?php the_title(); ?></h2>
+    <?php the_content(); ?>
+    
+  <?php }
+
+  get_footer();
+
 ?>
