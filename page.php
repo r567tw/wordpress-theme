734ba0a3b924b4aae54060551d1b0c4c75134210
r567tw <r567tw@gmail.com>
2023-07-24 23:06:37 +0800

    Parent & Children Pages

--- page.php
+++ page.php
@@ -15,12 +15,14 @@
       </div>
     </div>
 
-    <div class="container container--narrow page-section">
-      <div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
-        </p>
-      </div>
+    <?php if ($parent = wp_get_post_parent_id(get_the_ID())){ ?>
+      <div class="container container--narrow page-section">
+        <div class="metabox metabox--position-up metabox--with-home-link">
+          <p>
+            <a class="metabox__blog-home-link" href="<?=get_permalink($parent)?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?=get_the_title($parent)?></a> <span class="metabox__main"><?=the_title()?></span>
+          </p>
+        </div>
+    <?php } ?>
 <!-- 
       <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
