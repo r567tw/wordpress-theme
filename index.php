ea81360f7bcfa9916e53f8b976b82594569ae04e
r567tw <r567tw@gmail.com>
2023-07-27 15:32:30 +0800

    update

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         
         <div class="metabox">
-          <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.F'); ?> in <?php echo get_the_category_list(' and '); ?></p>
+          <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('F.j Y'); ?> in <?php echo get_the_category_list(' and '); ?></p>
         </div>
 
         <div class="generic-content">
