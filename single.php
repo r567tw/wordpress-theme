08a8ec9df0b38cd950dacf3cc03218921c1d6d8b
r567tw <r567tw@gmail.com>
2023-07-27 15:03:06 +0800

    Blog

--- single.php
+++ single.php
@@ -1,14 +1,29 @@
-<?php
-  
-  get_header();
+<!-- For General Blog Template -->
+<?php get_header(); ?>
+    <div class="container container--narrow page-section">
+      <!-- Hello world! -->
+      <?php
+        while (have_posts()){
+          the_post();
+          echo the_title();
+      ?>
+      <div class="post-item">
+        <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        
+        <div class="metabox">
+          <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.F'); ?> in <?php echo get_the_category_list(' and '); ?></p>
+        </div>
 
-  while(have_posts()) {
-    the_post(); ?>
-    <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>
-    
-  <?php }
+        <div class="generic-content">
+          <?php the_content(); ?>
+        </div>
+      </div>
+      <?php
+        }
+      ?>
+    </div>
 
-  get_footer();
+
+  <?php get_footer();
 
 ?>
